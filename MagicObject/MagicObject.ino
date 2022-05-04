65339b1689f9d548b565994d9c24ed8908030023
tdilges <torben.dilges@muk.thm.de>
2022-05-04 21:38:47 +0200

    LED dynamisch ansprechbar, WLAN-Monitor-Output optimiert
    LED dynamisch ansprechbar, WLAN-Monitor-Output optimiert

--- MagicObject/MagicObject.ino
+++ MagicObject/MagicObject.ino
@@ -2,67 +2,81 @@
 #include "Arduino.h"
 #include "HardwareSerial.h"
 #include <WiFi.h>
+//#include <Adafruit_NeoPixel.h>
 
-
+// LED Settings
 #define LEDS_COUNT  1
 #define LEDS_PIN	48
 #define CHANNEL		0
 
-#define NETWORK_NAME "Hogwarts"
-#define PASSWORD "alohomora"
+// Farbkonstanten-definitionen
 
+/* to-do: Vanessa, Maren, Katharina
+
+Bitte hier sinnvolle Konstanten für Farben inkl. deren Farbcodes definieren!
+
+*/
+
+//Router / WIFI Settings:
+#define NETWORK_NAME "Hogwarts" // entsprechend anpassen
+#define PASSWORD "alohomora"    // entsprechend anpassen
+
+
+//Methoden-Deklaration
+void setColor(int color);
 Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
 
+
+//BOOTWARE
 void setup() {
- 
- /* Logik zum Clearen des Serial Monitors -> Funzt noch nicht
-  Serial.write(27);       // ESC command
-  Serial.println("[2J");    // clear screen command
-  Serial.write(27);
-  Serial.println("[H");     // cursor to home command
-  */ //Clear-Logik Ende
-  Serial.begin(9600);
-  ScanWiFiNetwork();
+   Serial.begin(9600);
+ // ScanWiFiNetwork();
+  Serial.println("\nSetup start ...");
   connectToWiFi();
   strip.begin();
-  strip.setBrightness(20);
-    
+  setColor(170);
+  strip.setBrightness(100); 
 }
 
 void loop() {
-led();
-
+Serial.println("..............................");
+Serial.println("Farbe wird angepasst.");
+setColor(85);
+Serial.println("Farbe erfolgreich angepasst.");
+strip.setBrightness(10); 
+Serial.println("Regulaerer Betriebsmodus aktiv.");
 }
 
-
-
-void led(){
+// Set Color
+void setColor(int color){
 
     for (int j = 0; j < 255; j += 2) {
     for (int i = 0; i < LEDS_COUNT; i++) {
-      strip.setLedColorData(i, strip.Wheel((i * 256 / LEDS_COUNT + j) & 255)); // -> Liefert einen 8-Bit Zahlenwert an das Board (256 Werte, 0-255)
+      //strip.setLedColorData(i, strip.Wheel((i * 256 / LEDS_COUNT + j) & 255)); // -> Liefert einen 8-Bit Zahlenwert an das Board (256 Werte, 0-255)
+      strip.setLedColorData(i, strip.Wheel(color));
     }
     
     strip.show();
-    delay(500);
   }  
 }
 
-// mit WLAN Connecten
+
+// WLAN Connection
 void connectToWiFi() {
   WiFi.mode(WIFI_STA); //WIFI-Einstellungen statisch sind.
   WiFi.begin(NETWORK_NAME, PASSWORD);
-  Serial.println("Connecting to WiFi ..");
+  Serial.println("WLAN Verbindung wird hergestellt. Bitte warten ..");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
-    // led(0, 255, 0);
     delay(1000);
   }
-  Serial.println(WiFi.localIP());
+  Serial.print("\nWLAN Verbindung hergestellt. Die IP-Adresse des Magic Objects lautet: ");
+  Serial.print(WiFi.localIP());
+  setColor(170);
 }
 
-
-
+/*
+// Netzwerk Scan 
 void ScanWiFiNetwork(){
   Serial.println("scan start");
 
@@ -91,3 +105,4 @@ void ScanWiFiNetwork(){
   // Wait a bit before scanning again
   delay(5000);
 }
+*/
