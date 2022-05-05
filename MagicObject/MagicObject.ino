43853db13509e9cc353dcfd7b515319f36c88b2c
tdilges <torben.dilges@muk.thm.de>
2022-05-05 20:48:20 +0200

    LED / WIFI updates
    setColor edited.
    checkWifiStatus added.

--- MagicObject/MagicObject.ino
+++ MagicObject/MagicObject.ino
@@ -2,7 +2,7 @@
 #include "Arduino.h"
 #include "HardwareSerial.h"
 #include <WiFi.h>
-//#include <Adafruit_NeoPixel.h>
+//#include <Adafruit_NeoPixel.h> mal reinschauen
 
 // LED Settings
 #define LEDS_COUNT  1
@@ -15,15 +15,29 @@
 
 Bitte hier sinnvolle Konstanten für Farben inkl. deren Farbcodes definieren!
 
+keine Farbnamen, sondern fehler-funktionalitäten
+eher setStat statt setColor
+
+
+
+Notizen 05.05.
+zeitvar -> letzter run und aktuelle zeit
+vergleich zeitdifferenz als delay-ersatz
+blocking operation
+starttimer millis(); nanos();
+
+
+
+
 */
 
 //Router / WIFI Settings:
-#define NETWORK_NAME "Hogwarts" // entsprechend anpassen
-#define PASSWORD "alohomora"    // entsprechend anpassen
+#define NETWORK_NAME "IBB_AudioAP" // entsprechend anpassen
+#define PASSWORD "JonnyFantastic#"    // entsprechend anpassen
 
 
-//Methoden-Deklaration
-void setColor(int color);
+//Deklarationen
+//void setColor(int color);
 Freenove_ESP32_WS2812 strip = Freenove_ESP32_WS2812(LEDS_COUNT, LEDS_PIN, CHANNEL, TYPE_GRB);
 
 
@@ -34,36 +48,44 @@ void setup() {
   Serial.println("\nSetup start ...");
   connectToWiFi();
   strip.begin();
-  setColor(170);
-  strip.setBrightness(100); 
+  setColor(40);
+  strip.show();
+  //strip.setBrightness(100);
+  delay(1000); 
+  Serial.println("\nSetup abgeschlossen ...");
 }
 
 void loop() {
-Serial.println("..............................");
-Serial.println("Farbe wird angepasst.");
-setColor(85);
-Serial.println("Farbe erfolgreich angepasst.");
-strip.setBrightness(10); 
-Serial.println("Regulaerer Betriebsmodus aktiv.");
+//Serial.println("..............................");
+//Serial.println("Farbe wird angepasst.");
+//setColor(40);
+//strip.show();
+//Serial.println("Farbe erfolgreich angepasst.");
+//strip.setBrightness(10); //dokumentation checken
+//Serial.println("Regulaerer Betriebsmodus aktiv.");
+//delay(500);
+//setColor(255);
+//strip.show();
+//delay(500);
+checkWiFistatus();
+
 }
 
 // Set Color
 void setColor(int color){
-
-    for (int j = 0; j < 255; j += 2) {
-    for (int i = 0; i < LEDS_COUNT; i++) {
-      //strip.setLedColorData(i, strip.Wheel((i * 256 / LEDS_COUNT + j) & 255)); // -> Liefert einen 8-Bit Zahlenwert an das Board (256 Werte, 0-255)
-      strip.setLedColorData(i, strip.Wheel(color));
-    }
+//Serial.println("setcolor");
     
-    strip.show();
-  }  
+      strip.setLedColorData(0, strip.Wheel(color));
+    
+    
+        //strip.show();
+        //strip.show(); //dokumentation checken
 }
 
 
 // WLAN Connection
 void connectToWiFi() {
-  WiFi.mode(WIFI_STA); //WIFI-Einstellungen statisch sind.
+  WiFi.mode(WIFI_STA); //WIFI-Einstellungen sind statisch.
   WiFi.begin(NETWORK_NAME, PASSWORD);
   Serial.println("WLAN Verbindung wird hergestellt. Bitte warten ..");
   while (WiFi.status() != WL_CONNECTED) {
@@ -71,9 +93,47 @@ void connectToWiFi() {
     delay(1000);
   }
   Serial.print("\nWLAN Verbindung hergestellt. Die IP-Adresse des Magic Objects lautet: ");
-  Serial.print(WiFi.localIP());
-  setColor(170);
+  Serial.println(WiFi.localIP());
+  Serial.print("MAC-Adresse: ");
+  Serial.println(WiFi.macAddress());
 }
+
+
+
+void checkWiFistatus () {
+  Serial.println("Ab jetzt wird Wlan gechecked");
+  delay(10000);
+  
+  /* Wenn Status gleich 3, 
+  dann leuchte blau 
+  ansonsten
+  wenn Status nicht gleich 3
+  dann leuchte rot*/
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Status ist nicht 3 = Nicht verbunden");
+    //strip.begin();
+    setColor(255);
+    strip.show();
+    Serial.println("Es sollte jetzt rot leuchten");
+  }
+  else (WiFi.status() == WL_CONNECTED); { // if geht auch
+    Serial.println("Status ist 3 = Verbunden");
+    //strip.begin();
+    setColor(170);
+    strip.show();
+    Serial.println("Es sollte jetzt blau leuchten");
+    Serial.print("\nWLAN Verbindung hergestellt. Die IP-Adresse des Magic Objects lautet: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("MAC-Adresse: ");
+    Serial.println(WiFi.macAddress());
+  }
+}
+
+
+
+
+
+
 
 /*
 // Netzwerk Scan 
